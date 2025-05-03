6ace4407feef91dc1378af52a049984a59e4f8fa
Rachel Hall <=>
2025-05-03 09:31:30 -0700

    add image gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import image from '@astrojs/image';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), image()]
 });
